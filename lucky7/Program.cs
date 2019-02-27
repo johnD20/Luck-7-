c4bd191a48db4a62bb18aa4d49f33b8e429e694f
johnD20 <32309794+johnD20@users.noreply.github.com>
2019-02-27 14:34:59 -0700

    commit

---
+++ lucky7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lucky7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
